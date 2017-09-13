7d5d681ea444b9aad6c9487f6d4c8dcd59fd815c
Giovani Generali <giovani.generali@gmail.com>
2017-09-13 10:21:20 -0300

    update port

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use((0, _cors2.default)());
 
 // set constants
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || 'localhost');
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 app.set('site', 'https://github.com/wgenial/consulta-placa');
 
 // default and redirect to github project
