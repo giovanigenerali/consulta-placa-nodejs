cd7bafac570a48d379597b2cb71df2ea806506d2
Giovani Generali <giovani.generali@gmail.com>
2017-12-22 21:34:43 -0200

    code style

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-var express = require('express');
-var sinesp = require('sinesp-nodejs');
+var express = require("express");
+var sinesp = require("sinesp-nodejs");
 var app = express();
 
-app.set('port', process.env.PORT || 8080);
-app.set('ip', process.env.IP || '0.0.0.0');
-app.set('site', 'https://github.com/wgenial/consulta-placa');
+app.set("port", process.env.PORT || 8080);
+app.set("ip", process.env.IP || "0.0.0.0");
+app.set("site", "https://github.com/wgenial/consulta-placa");
 
 app.get("/", function(req, res, next) {
-  res.redirect(app.get('site'));
+  res.redirect(app.get("site"));
 });
 
 app.get("/:plate", function(req, res, next) {
-  var plate = req.params.plate.replace(/\W/g,'');
+  var plate = req.params.plate.replace(/\W/g,"");
   sinesp.consultaPlaca(plate, function(response) {
     res.json(response);
   });
 });
 
-app.listen(app.get('port'), app.get('ip'), function() {
-  return console.log("Listening on " + app.get('port') + "\nPress CTRL-C to stop server.");
+app.listen(app.get("port"), app.get("ip"), function() {
+  return console.log("Listening on " + app.get("port") + "\nPress CTRL-C to stop server.");
 });
 
 exports = module.exports = app;
